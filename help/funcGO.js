cdec929d922edf0bce5b3260896585de434fd8f8
Nadspink <artyr20101@gmail.com>
2017-03-12 16:19:19 +0300

    New lin

---
+++ help/funcGO.js
@@ -0,0 +1,4 @@
+function funcGOv()
+{
+    alert("sank for you quest")
+}
---
+++ help/manyGO.js
@@ -0,0 +1,17 @@
+function funcMANY()
+{
+    var many= document.getElementById('many').value;
+    if(many<0)
+        {
+        alert("you bad man");
+        }
+    else if(many>100)
+        {
+            alert("you Fu*kin milioner");
+        }
+     else if(many>0)
+        {
+            alert("not bad");
+        }
+   
+}
